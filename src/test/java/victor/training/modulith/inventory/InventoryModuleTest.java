0087c369a1b2c2a48d36318f5bf21642a7096912
Victor Rentea <victorrentea@gmail.com>
2023-10-23 22:03:24 +0300

    polish

--- src/test/java/victor/training/modulith/inventory/InventoryModuleTest.java
+++ src/test/java/victor/training/modulith/inventory/InventoryModuleTest.java
@@ -2,6 +2,6 @@ package victor.training.modulith.inventory;
 
 import org.springframework.modulith.test.ApplicationModuleTest;
 
-@ApplicationModuleTest
+@ApplicationModuleTest // instead of the @SpringBootTest
 public class InventoryModuleTest {
 }
