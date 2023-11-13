0cd5fc3d08b09ed62dd2e78d8ee98f7111ba0b54
Victor Rentea <victorrentea@gmail.com>
2023-11-13 22:49:40 +0200

    tweak

--- src/main/java/victor/training/modulith/ModulithApp.java
+++ src/main/java/victor/training/modulith/ModulithApp.java
@@ -4,9 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.FullyQualifiedAnnotationBeanNameGenerator;
 
 @EnableFeignClients
-@SpringBootApplication
+@SpringBootApplication(nameGenerator = FullyQualifiedAnnotationBeanNameGenerator.class) // allow classes with same name in different packages
 @ConfigurationPropertiesScan
 public class ModulithApp {
 
