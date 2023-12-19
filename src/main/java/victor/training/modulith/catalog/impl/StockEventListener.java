e3af96b5fc9ec69de18b92f095381a8d378ef924
Victor Rentea <victorrentea@gmail.com>
2023-12-19 12:12:33 +0200

    eventual consistency, since the handler runs in a separate thread + transaction

--- src/main/java/victor/training/modulith/catalog/impl/StockEventListener.java
+++ src/main/java/victor/training/modulith/catalog/impl/StockEventListener.java
@@ -3,6 +3,7 @@ package victor.training.modulith.catalog.impl;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import victor.training.modulith.inventory.ItemBackInStockEvent;
@@ -15,6 +16,7 @@ public class StockEventListener {
   private final ProductRepo productRepo;
 
   @EventListener
+  @Async // separate thread, separate tx. God help us all.
   @Transactional
   public void on(ItemRanOutOfStockEvent event) {
     Product product = productRepo.findById(event.productId()).orElseThrow();
@@ -22,6 +24,7 @@ public class StockEventListener {
   }
 
   @EventListener
+  @Async // separate thread, separate tx. God help us all.
   @Transactional
   public void on(ItemBackInStockEvent event) {
     Product product = productRepo.findById(event.productId()).orElseThrow();
