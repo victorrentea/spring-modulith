0d9f3296e0c52b13ba7f53b6c7e30a0a5eef243a
Victor Rentea <victorrentea@gmail.com>
2023-12-20 01:15:33 +0200

    Revert "problems of async: preventing race bugs: pessimistic or optimistic"
    This reverts commit 76e8e83fde86ba464b4b3fc2729ce83587ec1fa6.

--- src/main/java/victor/training/modulith/catalog/impl/StockEventListener.java
+++ src/main/java/victor/training/modulith/catalog/impl/StockEventListener.java
@@ -3,7 +3,6 @@ package victor.training.modulith.catalog.impl;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
-import org.springframework.modulith.ApplicationModuleListener;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -17,17 +16,9 @@ public class StockEventListener {
   private final ProductRepo productRepo;
 
   @EventListener
-//  @Async // separate thread, separate tx. God help us all.
-//  @Transactional
-  @ApplicationModuleListener // spring
-  // SpringModulith
-  // saves in a DB table the event  when publshed in the tx of the publisher.
-  // then with a scheduler, tries to dispatch these events, deleting them afterwards
-
-  // USing a separate queue
-  // we are doing that for 20years.
-  // it's "Service Activator" JavaEE pattern : send ourselves a message over rabbit/JMS durable queue
-  public synchronized void on(ItemRanOutOfStockEvent event) {
+  @Async // separate thread, separate tx. God help us all.
+  @Transactional
+  public void on(ItemRanOutOfStockEvent event) {
     Product product = productRepo.findById(event.productId()).orElseThrow();
     product.inStock(false);
   }
