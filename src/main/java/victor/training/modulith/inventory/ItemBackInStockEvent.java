ca20c68387b87a02a74c73ad880ca82b8e3cd67d
Victor Rentea <victorrentea@gmail.com>
2023-12-19 12:08:29 +0200

    Todo3: done with replication, while still 100% fullly consistent.

---
+++ src/main/java/victor/training/modulith/inventory/ItemBackInStockEvent.java
@@ -0,0 +1,4 @@
+package victor.training.modulith.inventory;
+
+public record ItemBackInStockEvent(long productId) {
+}
---
+++ src/main/java/victor/training/modulith/inventory/RanOutOfStockEvent.java
@@ -0,0 +1,4 @@
+package victor.training.modulith.inventory;
+
+public record RanOutOfStockEvent(long productId) {
+}
