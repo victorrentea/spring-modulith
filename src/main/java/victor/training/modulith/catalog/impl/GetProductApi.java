2aef04eafb415a8f5e27ee6be57708f53f181aa1
Victor Rentea <victorrentea@gmail.com>
2023-12-19 12:00:09 +0200

    todo2: alternatives: query 2nd module, have a 'aggregator' higher module, or microfrontends

--- src/main/java/victor/training/modulith/catalog/impl/GetProductApi.java
+++ src/main/java/victor/training/modulith/catalog/impl/GetProductApi.java
@@ -13,6 +13,9 @@ public class GetProductApi {
   private final ProductRepo productRepo;
   private final InventoryModule inventoryModule;
 
+  // Think> why does my module provide in its REST API data from another module?
+  //a) a BFF/proxy in front
+  //b) include in your product page a dedicated <stock> webcomponent developed by the inventory full-stack team
   public record GetProductResponse(long id,
                                        String name,
                                        String description,
@@ -29,5 +32,7 @@ public class GetProductApi {
         stock,
         product.price());
   }
+  // use a queue with commands to READ DATA - NEVER DO THAT
+  // to read data always use a sync call (method call, REST GET in microservices)
 
 }
