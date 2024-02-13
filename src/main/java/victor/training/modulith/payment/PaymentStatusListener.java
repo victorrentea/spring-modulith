7ee79af6afac8416dfc53c538bc2d29ac2a8af8a
Victor Rentea <victorrentea@gmail.com>
2024-02-13 15:47:51 +0200

    dip vs events

--- src/main/java/victor/training/modulith/payment/PaymentStatusListener.java
+++ src/main/java/victor/training/modulith/payment/PaymentStatusListener.java
@@ -3,5 +3,7 @@ package victor.training.modulith.payment;
 public interface PaymentStatusListener { // dependency inversion
 
   void paymentConfirmed(long orderId, boolean ok);
-  // this allows the other module you call to return you results, unlike events
+  // dependency inversion allows the other
+  // module you call to return you results,
+  // unlike events
 }
