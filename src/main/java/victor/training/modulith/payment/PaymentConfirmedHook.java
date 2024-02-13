78380c7329c0b5effa34324d8dde47e9d8ab270f
Victor Rentea <victorrentea@gmail.com>
2024-02-13 15:45:29 +0200

    task1 = done

--- src/main/java/victor/training/modulith/payment/PaymentConfirmedHook.java
+++ src/main/java/victor/training/modulith/payment/PaymentConfirmedHook.java
@@ -1,5 +1,7 @@
 package victor.training.modulith.payment;
 
-public interface PaymentConfirmedHook {
+public interface PaymentConfirmedHook { // dependency inversion
+
   void paymentConfirmed(long orderId, boolean ok);
+  // this allows the other module you call to return you results, unlike events
 }
