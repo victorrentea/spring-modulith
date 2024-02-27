aefe571fcbf5356a125b099d01c3e9cc7af74e64
Victor Rentea <victorrentea@gmail.com>
2024-02-27 16:35:01 +0200

    // 2) + this Dep Inversion has to translation to a microservice ecosystem // this is just a purely OOP hack to revert some code dependency

--- src/main/java/victor/training/modulith/order/PaymentDoor.java
+++ src/main/java/victor/training/modulith/order/PaymentDoor.java
@@ -5,5 +5,8 @@ public interface PaymentDoor {
   String generatePaymentUrl(long orderId, double total);
 }
 
-// in general dendency inversion we do to DEFEND against a filthy adapter.
+//1) in general dendency inversion we do to DEFEND against a filthy adapter.
 // The contract (interface) is CLEAN while the implementation is "dirty"
+
+// 2) + this Dep Inversion has to translation to a microservice ecosystem
+// this is just a purely OOP hack to revert some code dependency
