36c52ad6eb711edee0ce7715353c53180ae466b1
Victor Rentea <victorrentea@gmail.com>
2024-02-27 16:33:52 +0200

    controversy: is this interface better? or is it just test-induced?

--- src/main/java/victor/training/modulith/order/PaymentDoor.java
+++ src/main/java/victor/training/modulith/order/PaymentDoor.java
@@ -1,6 +1,9 @@
 package victor.training.modulith.order;
 
+// isn't this test-induced design damage?
 public interface PaymentDoor {
   String generatePaymentUrl(long orderId, double total);
-
 }
+
+// in general dendency inversion we do to DEFEND against a filthy adapter.
+// The contract (interface) is CLEAN while the implementation is "dirty"
