3f2c486627fb4c00df88f61cce5e46b7ec1ca4c4
Victor Rentea <victorrentea@gmail.com>
2024-03-13 17:53:02 +0200

    fix compilation. introduce pagination

--- src/main/java/victor/training/modulith/catalog/impl/ProductRepo.java
+++ src/main/java/victor/training/modulith/catalog/impl/ProductRepo.java
@@ -1,11 +1,11 @@
 package victor.training.modulith.catalog.impl;
 
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface ProductRepo extends JpaRepository<Product, Long> {
-  List<Product> searchByNameLikeIgnoreCase(String namePart);
-//  List<Product> searchByNameLikeIgnoreCaseAndInStockTrue(String namePart);
+  List<Product> searchByNameLikeIgnoreCase(String namePart, PageRequest pageRequest);
+//  List<Product> searchByNameLikeIgnoreCaseAndInStockTrue(String namePart, PageRequest pageRequest);
 }
--- src/main/java/victor/training/modulith/catalog/impl/SearchProductApi.java
+++ src/main/java/victor/training/modulith/catalog/impl/SearchProductApi.java
@@ -1,6 +1,7 @@
 package victor.training.modulith.catalog.impl;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,9 +17,11 @@ public class SearchProductApi {
   }
 
   @GetMapping("catalog/search")
-  public List<ProductSearchResult> search(@RequestParam String name) {
-    // TODO search should only return items in stock
-    return productRepo.searchByNameLikeIgnoreCase(name).stream()
+  public List<ProductSearchResult> search(@RequestParam String name,
+                                          @RequestParam PageRequest pageRequest) {
+    // TODO only return items in stock
+    return productRepo.searchByNameLikeIgnoreCase(name, pageRequest)
+        .stream()
         .map(e -> new ProductSearchResult(e.id(), e.name()))
         .toList();
   }
