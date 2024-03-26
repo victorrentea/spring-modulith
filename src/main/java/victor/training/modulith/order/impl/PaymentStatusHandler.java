618608b717462366060b11cdbe55d055b79f6148
Victor Rentea <victorrentea@gmail.com>
2024-03-26 13:26:52 +0200

    prepare for multi-module maven project

--- src/main/java/victor/training/modulith/order/impl/PaymentStatusHandler.java
+++ src/main/java/victor/training/modulith/order/impl/PaymentStatusHandler.java
@@ -3,14 +3,8 @@ package victor.training.modulith.order.impl;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
-import org.springframework.modulith.ApplicationModuleListener;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Propagation;
-import org.springframework.transaction.annotation.Transactional;
 import victor.training.modulith.order.OrderStatus;
-import victor.training.modulith.order.impl.Order;
-import victor.training.modulith.order.impl.OrderRepo;
 import victor.training.modulith.payment.PaymentCompletedEvent;
 import victor.training.modulith.shipping.in.api.ShippingModule;
 
