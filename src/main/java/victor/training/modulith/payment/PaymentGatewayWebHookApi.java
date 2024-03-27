cba6aa17c32441c861aec38e8bb7ebfb990b4ab2
Victor Rentea <victorrentea@gmail.com>
2024-03-27 17:40:13 +0200

    cycle

--- src/main/java/victor/training/modulith/payment/PaymentGatewayWebHookApi.java
+++ src/main/java/victor/training/modulith/payment/PaymentGatewayWebHookApi.java
@@ -1,30 +1,22 @@
-package victor.training.modulith.order.impl;
+package victor.training.modulith.payment;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import victor.training.modulith.order.OrderStatus;
-import victor.training.modulith.shipping.in.api.ShippingModule;
+import victor.training.modulith.order.PaymentConfirmedHandler;
 
 @RestController
 @RequiredArgsConstructor
 // Webhook = a call back to me over HTTP
 public class PaymentGatewayWebHookApi { // TODO move to 'payment' module
-  private final OrderRepo orderRepo;
-  private final ShippingModule shippingModule;
+  private final PaymentConfirmedHandler paymentConfirmedHandler;
 
   @PutMapping("payment/{orderId}/status")
   public String confirmPayment(@PathVariable long orderId, @RequestBody boolean ok) {
-    Order order = orderRepo.findById(orderId).orElseThrow();
-    order.paid(ok);
-    if (order.status() == OrderStatus.PAYMENT_APPROVED) {
-      String trackingNumber = shippingModule.requestShipment(order.id(), order.shippingAddress());
-      order.scheduleForShipping(trackingNumber);
-    }
-    orderRepo.save(order);
-    System.out.println("Exit");
+    paymentConfirmedHandler.confirmPayment(orderId, ok);
     return "Payment callback received";
   }
 }
+
