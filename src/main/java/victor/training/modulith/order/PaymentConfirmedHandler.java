10b0e1e19f5bd396ddc79dafeb079f43a803e4d0
Victor Rentea <victorrentea@gmail.com>
2024-03-27 17:44:59 +0200

    Events between modules

--- src/main/java/victor/training/modulith/order/PaymentConfirmedHandler.java
+++ src/main/java/victor/training/modulith/order/PaymentConfirmedHandler.java
@@ -1,20 +1,23 @@
 package victor.training.modulith.order;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Service;
 import victor.training.modulith.order.impl.Order;
 import victor.training.modulith.order.impl.OrderRepo;
+import victor.training.modulith.payment.PaymentCompletedEvent;
 import victor.training.modulith.shipping.in.api.ShippingModule;
 
 @Service
 @RequiredArgsConstructor
-public class PaymentConfirmedHandler implements victor.training.modulith.payment.IPaymentConfirmedHandler {
+public class PaymentConfirmedHandler  {
   private final OrderRepo orderRepo;
   private final ShippingModule shippingModule;
-  @Override
-  public void confirmPayment(long orderId, boolean ok) {
-    Order order = orderRepo.findById(orderId).orElseThrow();
-    order.paid(ok);
+  @EventListener
+
+  public void confirmPayment(PaymentCompletedEvent event) {
+    Order order = orderRepo.findById(event.orderId()).orElseThrow();
+    order.paid(event.ok());
     if (order.status() == OrderStatus.PAYMENT_APPROVED) {
       String trackingNumber = shippingModule.requestShipment(order.id(), order.shippingAddress());
       order.scheduleForShipping(trackingNumber);
