51894957eec53ea8b55f9e7dc2430911ae2eda07
Victor Rentea <victorrentea@gmail.com>
2024-03-27 17:44:59 +0200

    Events between modules

--- src/main/java/victor/training/modulith/payment/PaymentGatewayWebHookApi.java
+++ src/main/java/victor/training/modulith/payment/PaymentGatewayWebHookApi.java
@@ -1,6 +1,7 @@
 package victor.training.modulith.payment;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,11 +11,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 // Webhook = a call back to me over HTTP
 public class PaymentGatewayWebHookApi { // TODO move to 'payment' module
-  private final IPaymentConfirmedHandler paymentConfirmedHandler;
+//  private final IPaymentConfirmedHandler paymentConfirmedHandler;
 
+  private final ApplicationEventPublisher eventPublisher;
   @PutMapping("payment/{orderId}/status")
   public String confirmPayment(@PathVariable long orderId, @RequestBody boolean ok) {
-    paymentConfirmedHandler.confirmPayment(orderId, ok);
+//    paymentConfirmedHandler.confirmPayment(orderId, ok);
+    eventPublisher.publishEvent(new PaymentCompletedEvent(orderId, ok));
     return "Payment callback received";
   }
 }
