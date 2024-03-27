bcf4c6403274d8c86454941e5390230e1a634253
Victor Rentea <victorrentea@gmail.com>
2024-03-27 17:35:01 +0200

    fetched stock via public module api

--- src/main/java/victor/training/modulith/catalog/impl/GetProductApi.java
+++ src/main/java/victor/training/modulith/catalog/impl/GetProductApi.java
@@ -4,6 +4,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import victor.training.modulith.inventory.InventoryModule;
+import victor.training.modulith.inventory.impl.Stock;
+import victor.training.modulith.inventory.impl.StockRepo;
 
 @RestController
 @RequiredArgsConstructor
@@ -13,16 +16,17 @@ public class GetProductApi {
   public record GetProductResponse(long id,
                             String name,
                             String description,
-//                            int stock, // TODO display stock in product page UI
+                            int stock, // TODO display stock in product page UI
                             double price) {}
-
+private final InventoryModule inventoryModule;
   @GetMapping("catalog/{productId}")
   public GetProductResponse getProduct(@PathVariable long productId) {
     Product product = productRepo.findById(productId).orElseThrow();
+    int stock = inventoryModule.getAvailableStock(productId);
     return new GetProductResponse(product.id(),
         product.name(),
         product.description(),
-//        stock,
+        stock,
         product.price());
   }
 }
