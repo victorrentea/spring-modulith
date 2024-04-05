9b705f63d0f667227d95604d0da623f1903e6b0b
Victor Rentea <victorrentea@gmail.com>
2024-04-05 13:12:07 +0200

    Revert "Events between modules"
    This reverts commit 10b0e1e19f5bd396ddc79dafeb079f43a803e4d0.

--- src/main/java/victor/training/modulith/payment/PaymentGatewayWebHookApi.java
+++ src/main/java/victor/training/modulith/payment/PaymentGatewayWebHookApi.java
@@ -1,7 +1,6 @@
 package victor.training.modulith.payment;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,13 +10,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 // Webhook = a call back to me over HTTP
 public class PaymentGatewayWebHookApi { // TODO move to 'payment' module
-//  private final IPaymentConfirmedHandler paymentConfirmedHandler;
+  private final IPaymentConfirmedHandler paymentConfirmedHandler;
 
-  private final ApplicationEventPublisher eventPublisher;
   @PutMapping("payment/{orderId}/status")
   public String confirmPayment(@PathVariable long orderId, @RequestBody boolean ok) {
-//    paymentConfirmedHandler.confirmPayment(orderId, ok);
-    eventPublisher.publishEvent(new PaymentCompletedEvent(orderId, ok));
+    paymentConfirmedHandler.confirmPayment(orderId, ok);
     return "Payment callback received";
   }
 }
