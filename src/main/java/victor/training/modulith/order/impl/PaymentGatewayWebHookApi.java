987005a6a907c227019bff2825f87145707f2c35
Victor Rentea <victorrentea@gmail.com>
2024-04-05 13:12:07 +0200

    Revert "cycle"
    This reverts commit 419deacacb348269b7e3c428875c07a400e37fc1.

--- src/main/java/victor/training/modulith/order/impl/PaymentGatewayWebHookApi.java
+++ src/main/java/victor/training/modulith/order/impl/PaymentGatewayWebHookApi.java
@@ -1,22 +1,30 @@
-package victor.training.modulith.payment;
+package victor.training.modulith.order.impl;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import victor.training.modulith.order.PaymentConfirmedHandler;
+import victor.training.modulith.order.OrderStatus;
+import victor.training.modulith.shipping.in.api.ShippingModule;
 
 @RestController
 @RequiredArgsConstructor
 // Webhook = a call back to me over HTTP
 public class PaymentGatewayWebHookApi { // TODO move to 'payment' module
-  private final PaymentConfirmedHandler paymentConfirmedHandler;
+  private final OrderRepo orderRepo;
+  private final ShippingModule shippingModule;
 
   @PutMapping("payment/{orderId}/status")
   public String confirmPayment(@PathVariable long orderId, @RequestBody boolean ok) {
-    paymentConfirmedHandler.confirmPayment(orderId, ok);
+    Order order = orderRepo.findById(orderId).orElseThrow();
+    order.paid(ok);
+    if (order.status() == OrderStatus.PAYMENT_APPROVED) {
+      String trackingNumber = shippingModule.requestShipment(order.id(), order.shippingAddress());
+      order.scheduleForShipping(trackingNumber);
+    }
+    orderRepo.save(order);
+    System.out.println("Exit");
     return "Payment callback received";
   }
 }
-
--- src/main/java/victor/training/modulith/order/impl/PlaceOrderApi.java
+++ src/main/java/victor/training/modulith/order/impl/PlaceOrderApi.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import victor.training.modulith.order.CatalogModuleInterface;
 import victor.training.modulith.order.InventoryModuleInterface;
-import victor.training.modulith.payment.PaymentService;
 import victor.training.modulith.shared.LineItem;
 import victor.training.modulith.shipping.out.event.ShippingResultEvent;
 
