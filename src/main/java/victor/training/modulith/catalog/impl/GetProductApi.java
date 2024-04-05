b0f90d6660cf7481577bab6fdef64f7cf9483a8e
Victor Rentea <victorrentea@gmail.com>
2024-04-05 14:34:18 +0200

    I brutally accessed the ENtity and Repo of anoter module, braking its encapsulation

--- src/main/java/victor/training/modulith/catalog/impl/GetProductApi.java
+++ src/main/java/victor/training/modulith/catalog/impl/GetProductApi.java
@@ -4,25 +4,28 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import victor.training.modulith.inventory.impl.StockRepo;
 
 @RestController
 @RequiredArgsConstructor
 public class GetProductApi {
   private final ProductRepo productRepo;
+  private final StockRepo stockRepo;
 
   public record GetProductResponse(long id,
                             String name,
                             String description,
-//                            int stock, // TODO display stock in product page UI
+                            int stock, // TODO display stock in product page UI
                             double price) {}
 
   @GetMapping("catalog/{productId}")
   public GetProductResponse getProduct(@PathVariable long productId) {
     Product product = productRepo.findById(productId).orElseThrow();
+    Integer stock = stockRepo.findByProductId(productId).orElseThrow().items();
     return new GetProductResponse(product.id(),
         product.name(),
         product.description(),
-//        stock,
+        stock,
         product.price());
   }
 }
