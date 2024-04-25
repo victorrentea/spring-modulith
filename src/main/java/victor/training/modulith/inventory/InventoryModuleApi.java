6b23c3f06fdac0ce6a1e090f974233e9a8f4a079
Victor Rentea <victorrentea@gmail.com>
2024-04-25 19:02:05 +0200

    respected encapsulaton

--- src/main/java/victor/training/modulith/inventory/InventoryModuleApi.java
+++ src/main/java/victor/training/modulith/inventory/InventoryModuleApi.java
@@ -2,6 +2,8 @@ package victor.training.modulith.inventory;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import victor.training.modulith.inventory.model.Stock;
+import victor.training.modulith.inventory.repo.StockRepo;
 import victor.training.modulith.inventory.service.ReserveStockService;
 import victor.training.modulith.order.InventoryModuleInterface;
 import victor.training.modulith.shared.LineItem;
@@ -12,9 +14,14 @@ import java.util.List;
 @RequiredArgsConstructor
 public class InventoryModuleApi implements InventoryModuleInterface {
   private final ReserveStockService reserveStockService;
+  private final StockRepo stockRepo;
 
   public void reserveStock(long orderId, List<LineItem> items) {
     reserveStockService.reserveStock(orderId, items);
   }
 
+  public int getStock(long productId) {
+    return stockRepo.findByProductId(productId)
+        .map(Stock::items).orElse(0);
+  }
 }
