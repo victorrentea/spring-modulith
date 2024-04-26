fb739fc06eafd20af0e5f96a1dcf809e552ca1f7
Victor Rentea <victorrentea@gmail.com>
2024-04-26 14:44:44 +0200

    encapsulated access to private impl of order inside the order only

--- src/main/java/victor/training/modulith/payment/PaymentGatewayWebHookApi.java
+++ src/main/java/victor/training/modulith/payment/PaymentGatewayWebHookApi.java
@@ -1,11 +1,14 @@
-package victor.training.modulith.order.impl;
+package victor.training.modulith.payment;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import victor.training.modulith.order.OrderModuleApi;
 import victor.training.modulith.order.OrderStatus;
+import victor.training.modulith.order.impl.Order;
+import victor.training.modulith.order.impl.OrderRepo;
 import victor.training.modulith.shipping.in.api.ShippingModuleApi;
 
 @RestController
@@ -14,17 +17,14 @@ import victor.training.modulith.shipping.in.api.ShippingModuleApi;
 public class PaymentGatewayWebHookApi { // TODO move to 'payment' module
   private final OrderRepo orderRepo;
   private final ShippingModuleApi shippingModule;
+  private final OrderModuleApi orderModuleApi;
 
   @PutMapping("payment/{orderId}/status")
   public String confirmPayment(@PathVariable long orderId, @RequestBody boolean ok) {
-    Order order = orderRepo.findById(orderId).orElseThrow();
-    order.paid(ok);
-    if (order.status() == OrderStatus.PAYMENT_APPROVED) {
-      String trackingNumber = shippingModule.requestShipment(order.id(), order.shippingAddress());
-      order.scheduleForShipping(trackingNumber);
-    }
-    orderRepo.save(order);
+    orderModuleApi.confirmOrderPayment(orderId, ok);
     System.out.println("Exit");
     return "Payment callback received";
   }
+
+
 }
