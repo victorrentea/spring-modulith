318596ee7414f5bcd4a804fcc3f4cb133f86001a
Victor Rentea <victorrentea@gmail.com>
2024-04-26 14:22:55 +0200

    fetched stock politely via the inventory module's public internal api

--- src/main/java/victor/training/modulith/catalog/impl/GetProductApi.java
+++ src/main/java/victor/training/modulith/catalog/impl/GetProductApi.java
@@ -4,18 +4,20 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import victor.training.modulith.inventory.InventoryModuleApi;
 
 @RestController
 @RequiredArgsConstructor
 public class GetProductApi {
   private final ProductRepo productRepo;
+  private final InventoryModuleApi inventoryModuleApi;
   // example of Vertical Slice Architecture (VSA) - one class / API
 
   public record GetProductResponse(
       long id,
       String name,
       String description,
-      // int stock, // TODO display stock in product page UI
+       int stock, // TODO display stock in product page UI
       Double price,
       Double stars
       ) {
@@ -24,10 +26,11 @@ public class GetProductApi {
   @GetMapping("catalog/{productId}")
   public GetProductResponse execute(@PathVariable long productId) {
     Product product = productRepo.findById(productId).orElseThrow();
+    int stock= inventoryModuleApi.getStock(productId);
     return new GetProductResponse(product.id(),
         product.name(),
         product.description(),
-        // stock,
+         stock,
         product.price(),
         product.stars()
     );
