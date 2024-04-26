959c45c12f7d36ad15cfd09adf42f6113cf7ebcf
Victor Rentea <victorrentea@gmail.com>
2024-04-26 19:56:24 +0200

    hibernate @View :D

--- src/main/java/victor/training/modulith/inventory/StockView.java
+++ src/main/java/victor/training/modulith/inventory/StockView.java
@@ -3,12 +3,20 @@ package victor.training.modulith.inventory;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import lombok.Getter;
+import org.hibernate.annotations.Immutable;
+import org.hibernate.annotations.View;
 
 @Entity
+@View(query = """
+    select STOCK.PRODUCT_ID, STOCK.ITEMS as STOCK
+    from STOCK
+    """)
+@Getter
+@Immutable
 public class StockView {
   @Id
-  @Column(insertable = false)
-  private Long productId;
+  private long productId;
 
   private Integer stock;
 }
