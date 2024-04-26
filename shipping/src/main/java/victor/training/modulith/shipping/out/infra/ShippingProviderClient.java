6c5926ef2e3fd4eef7bd2b7086da8eea662cf094
Victor Rentea <victorrentea@gmail.com>
2024-04-26 19:27:45 +0200

    end

--- shipping/src/main/java/victor/training/modulith/shipping/out/infra/ShippingProviderClient.java
+++ shipping/src/main/java/victor/training/modulith/shipping/out/infra/ShippingProviderClient.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 @FeignClient("shipping-provider")
 public interface ShippingProviderClient {
   @PostMapping("create-shipping")
+//  @Timed
+//  @LogToKibana
   String requestShipment(@RequestParam String pickupAddress,
                          @RequestParam String deliveryAddress,
                          @RequestParam long orderId);
