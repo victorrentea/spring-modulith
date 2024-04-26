cbab41625e4b0049d86477999c0fdbb4f65fbd12
Victor Rentea <victorrentea@gmail.com>
2024-04-26 17:06:05 +0200

    done

--- src/main/java/victor/training/modulith/shared/api/inventory/StockView.java
+++ src/main/java/victor/training/modulith/shared/api/inventory/StockView.java
@@ -3,12 +3,15 @@ package victor.training.modulith.shared.api.inventory;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import org.hibernate.annotations.Immutable;
 
 @Entity
+@Immutable
 public class StockView {
   @Id
   @Column(insertable = false)
   private Long productId;
 
+  @Column(updatable = false)
   private Integer stock;
 }
