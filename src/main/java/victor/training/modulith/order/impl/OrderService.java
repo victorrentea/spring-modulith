56b3c40248268b459efc10ba98508f8f9a17f618
Victor Rentea <victorrentea@gmail.com>
2024-05-10 08:24:04 +0300

    cleanup, simplify for presentation

--- src/main/java/victor/training/modulith/order/impl/OrderService.java
+++ src/main/java/victor/training/modulith/order/impl/OrderService.java
@@ -1,52 +1,59 @@
 package victor.training.modulith.order.impl;
 
-import jakarta.validation.constraints.NotEmpty;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.modulith.ApplicationModuleListener;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Service;
 import victor.training.modulith.order.CatalogModuleInterface;
 import victor.training.modulith.order.InventoryModuleInterface;
+import victor.training.modulith.order.OrderStatus;
 import victor.training.modulith.shared.LineItem;
+import victor.training.modulith.shipping.in.api.ShippingModuleApi;
 import victor.training.modulith.shipping.out.event.ShippingResultEvent;
 
 import java.util.List;
 import java.util.Map;
 
 import static java.util.stream.Collectors.toMap;
+import static victor.training.modulith.order.impl.OrderRestApi.*;
 
 @Slf4j
-@RestController
+@Service
 @RequiredArgsConstructor
-public class PlaceOrderApi {
+public class OrderService {
   private final OrderRepo orderRepo;
   private final CatalogModuleInterface catalogModule;
   private final InventoryModuleInterface inventoryModule;
   private final PaymentService paymentService;
+  private final ShippingModuleApi shippingModule;
 
-  public record PlaceOrderRequest(
-      @NotEmpty String customerId,
-      @NotEmpty List<LineItem> items,
-      @NotEmpty String shippingAddress) {
+  public Order getOrder(long orderId) {
+    return orderRepo.findById(orderId).orElseThrow();
   }
 
-  @PostMapping("order")
-  public String placeOrder(@RequestBody @Validated PlaceOrderRequest request) {
+  public String doPlaceOrder(PlaceOrderRequest request) {
     List<Long> productIds = request.items().stream().map(LineItem::productId).toList();
     Map<Long, Double> prices = catalogModule.getManyPrices(productIds);
-    Map<Long, Integer> items = request.items.stream().collect(toMap(LineItem::productId, LineItem::count));
-    double totalPrice = request.items.stream().mapToDouble(e -> e.count() * prices.get(e.productId())).sum();
+    Map<Long, Integer> items = request.items().stream().collect(toMap(LineItem::productId, LineItem::count));
+    double totalPrice = request.items().stream().mapToDouble(e -> e.count() * prices.get(e.productId())).sum();
     Order order = new Order()
         .items(items)
-        .shippingAddress(request.shippingAddress)
-        .customerId(request.customerId)
+        .shippingAddress(request.shippingAddress())
+        .customerId(request.customerId())
         .total(totalPrice);
     orderRepo.save(order);
-    inventoryModule.reserveStock(order.id(), request.items);
+    inventoryModule.reserveStock(order.id(), request.items());
     return paymentService.generatePaymentUrl(order.id(), order.total());
+  }
+
+  public void onOrderPaid(long orderId, boolean ok) {
+    Order order = orderRepo.findById(orderId).orElseThrow();
+    order.paid(ok);
+    if (order.status() == OrderStatus.PAYMENT_APPROVED) {
+      String trackingNumber = shippingModule.requestShipment(order.id(), order.shippingAddress());
+      order.scheduleForShipping(trackingNumber);
+    }
+    orderRepo.save(order);
   }
 
   @ApplicationModuleListener
