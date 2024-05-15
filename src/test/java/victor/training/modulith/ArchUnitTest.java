aaca3ddb0e78acbb7a03aa246678cfa5d9871891
Victor Rentea <victorrentea@gmail.com>
2024-05-15 20:20:48 +0300

    disable test (for later)

--- src/test/java/victor/training/modulith/ArchUnitTest.java
+++ src/test/java/victor/training/modulith/ArchUnitTest.java
@@ -5,6 +5,7 @@ import com.tngtech.archunit.core.importer.ClassFileImporter;
 import com.tngtech.archunit.library.dependencies.SliceRule;
 import com.tngtech.archunit.library.dependencies.SlicesRuleDefinition;
 import com.tngtech.archunit.library.dependencies.syntax.GivenSlices;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -15,6 +16,7 @@ class ArchUnitTest {
   private JavaClasses classes = new ClassFileImporter().importPackages("victor.training.modulith");
 
   @Test
+  @Disabled ("For after the APIs are moved to shared")
   public void sharedApiIndependent() {
     SliceRule sliceRule = SlicesRuleDefinition.slices()
         .matching("victor.training.modulith.shared.api.(*)..*")
