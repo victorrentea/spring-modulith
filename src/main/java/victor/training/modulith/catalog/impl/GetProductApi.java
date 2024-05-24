6637c09d1db527e2f0b9ed2380e8b04a2019fd2d
Victor Rentea <victorrentea@gmail.com>
2024-05-24 15:16:00 +0300

    nu asa ca incalcam incapsularea modului inventar accesandu-i sfanta entitate interna.!!!!!

--- src/main/java/victor/training/modulith/catalog/impl/GetProductApi.java
+++ src/main/java/victor/training/modulith/catalog/impl/GetProductApi.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import victor.training.modulith.inventory.InventoryModuleApi;
+import victor.training.modulith.inventory.model.Stock;
+import victor.training.modulith.inventory.repo.StockRepo;
 
 @RestController
 @RequiredArgsConstructor
@@ -17,20 +19,21 @@ public class GetProductApi {
       long id,
       String name,
       String description,
-//       int stock, // TODO display stock in product page UI
+       int stock, // TODO display stock in product page UI
       Double price,
       Double stars
       ) {
   }
+  private final StockRepo stockRepo;
 
   @GetMapping("catalog/{productId}")
   public GetProductResponse execute(@PathVariable long productId) {
     Product product = productRepo.findById(productId).orElseThrow();
-    //int stock= TODO
+    int stock= stockRepo.findByProductId(productId).map(Stock::items).orElse(0);
     return new GetProductResponse(product.id(),
         product.name(),
         product.description(),
-//         stock,
+         stock,
         product.price(),
         product.stars()
     );
