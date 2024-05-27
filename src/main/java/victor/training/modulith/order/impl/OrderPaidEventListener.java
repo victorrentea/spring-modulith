419e1d54f553b6a1805383746a8168e7db5ddc21
Victor Rentea <victorrentea@gmail.com>
2024-05-27 14:27:51 +0300

    event

--- src/main/java/victor/training/modulith/order/impl/OrderPaidEventListener.java
+++ src/main/java/victor/training/modulith/order/impl/OrderPaidEventListener.java
@@ -1,22 +1,24 @@
-package victor.training.modulith.order;
+package victor.training.modulith.order.impl;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Service;
-import victor.training.modulith.order.impl.Order;
-import victor.training.modulith.order.impl.OrderRepo;
+import victor.training.modulith.order.OrderStatus;
+import victor.training.modulith.payment.OrderPaidEvent;
 import victor.training.modulith.shipping.in.api.ShippingModuleApi;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
-public class OrderModuleApi {
+public class OrderPaidEventListener {
   private final OrderRepo orderRepo;
   private final ShippingModuleApi shippingModule;
 
-   public void onOrderPaid(long orderId, boolean ok) {
-    Order order = orderRepo.findById(orderId).orElseThrow();
-    order.paid(ok);
+  @EventListener
+  public void onOrderPaid(OrderPaidEvent event) {
+    Order order = orderRepo.findById(event.orderId()).orElseThrow();
+    order.paid(event.ok());
     if (order.status() == OrderStatus.PAYMENT_APPROVED) {
       String trackingNumber = shippingModule.requestShipment(order.id(), order.shippingAddress());
       order.scheduleForShipping(trackingNumber);
--- src/main/java/victor/training/modulith/payment/PaymentGatewayWebHookApi.java
+++ src/main/java/victor/training/modulith/payment/PaymentGatewayWebHookApi.java
@@ -1,27 +1,23 @@
 package victor.training.modulith.payment;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import victor.training.modulith.order.OrderModuleApi;
-import victor.training.modulith.order.OrderStatus;
-import victor.training.modulith.order.impl.Order;
-import victor.training.modulith.order.impl.OrderRepo;
-import victor.training.modulith.shipping.in.api.ShippingModuleApi;
 
 @RestController
 @RequiredArgsConstructor
 // Webhook = a call back to me over HTTP
 public class PaymentGatewayWebHookApi { // TODO move to 'payment' module
-  private final OrderModuleApi orderModule;
+//  private final OrderModuleApi orderModule;
+  private final ApplicationEventPublisher eventPublisher;
   @PutMapping("payment/{orderId}/status")
   public String confirmPayment(@PathVariable long orderId, @RequestBody boolean ok) {
-    orderModule.onOrderPaid(orderId, ok);
+//    orderModule.onOrderPaid(orderId, ok);
+    eventPublisher.publishEvent(new OrderPaidEvent(orderId, ok));
     System.out.println("Exit");
     return "Payment callback received";
   }
-
-
 }
