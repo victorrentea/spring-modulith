27727d8d092c400dfcaffd142d75de48aa15bd5d
Victor Rentea <victorrentea@gmail.com>
2024-05-27 12:48:57 +0300

    direct dar prost ca nu respect incapsularea modului inventory

--- src/main/java/victor/training/modulith/catalog/impl/GetProductApi.java
+++ src/main/java/victor/training/modulith/catalog/impl/GetProductApi.java
@@ -4,7 +4,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-import victor.training.modulith.inventory.InventoryModuleApi;
+import victor.training.modulith.inventory.model.Stock;
+import victor.training.modulith.inventory.repo.StockRepo;
 
 @RestController
 @RequiredArgsConstructor
@@ -16,20 +17,23 @@ public class GetProductApi {
       long id,
       String name,
       String description,
-//       int stock, // TODO display stock in product page UI
+       int stock, // TODO display stock in product page UI
       Double price,
       Double stars
-      ) {
+  ) {
   }
+
+  private final StockRepo stockRepo;
 
   @GetMapping("catalog/{productId}")
   public GetProductResponse execute(@PathVariable long productId) {
     Product product = productRepo.findById(productId).orElseThrow();
-    //int stock= TODO
+    Stock entity = stockRepo.findByProductId(productId).orElseThrow();
+    int stock = entity.items();
     return new GetProductResponse(product.id(),
         product.name(),
         product.description(),
-//         stock,
+         stock,
         product.price(),
         product.stars()
     );
