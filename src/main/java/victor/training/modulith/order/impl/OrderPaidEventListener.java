4e1c6003b6a4e6a0f9f2e7065a3b98f1e3d4d58b
Victor Rentea <victorrentea@gmail.com>
2024-05-27 15:03:29 +0300

    event progression from method call to async persisted external events

--- src/main/java/victor/training/modulith/order/impl/OrderPaidEventListener.java
+++ src/main/java/victor/training/modulith/order/impl/OrderPaidEventListener.java
@@ -3,10 +3,16 @@ package victor.training.modulith.order.impl;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
+import org.springframework.modulith.ApplicationModuleListener;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.event.TransactionPhase;
+import org.springframework.transaction.event.TransactionalEventListener;
 import victor.training.modulith.order.OrderStatus;
 import victor.training.modulith.payment.OrderPaidEvent;
 import victor.training.modulith.shipping.in.api.ShippingModuleApi;
+
+import static org.springframework.transaction.event.TransactionPhase.AFTER_COMMIT;
 
 @Slf4j
 @Service
@@ -15,7 +21,21 @@ public class OrderPaidEventListener {
   private final OrderRepo orderRepo;
   private final ShippingModuleApi shippingModule;
 
-  @EventListener
+  // 1 = consistent si in sync
+  @EventListener // = executa toti listenerii in threadul [si tranzactia] publisherului, intr-o ordine ne-specificata
+  // + CONSISTENT - CODUL MAI GREU DE NAVIGAT = e practic un apel de metoda deghizat
+  // Folosesti @EventLIstener doar ca tranztie catre eventuri async
+
+  // 2
+//  @EventListener @Async // = executa listenerul in alt thread, independent de publisher; poate sa-ti dea race
+
+  //3
+//  @Async @TransactionalEventListener(phase = AFTER_COMMIT) // async dar duap COMMIT din publisher
+  // risk: server crash cat eventul tau inca nu a fost procesat sau eroare in procesarea eventului: eventul se pierde
+
+  //4 (spring-modulith) cam beta sa-l pui in prod
+//  @ApplicationModuleListener // ca mai sus, dar salvate intr-o tabela din DB
+  // sau mai bine sistem de mesaje extern: @KafkaListener, @RabbitListener
   public void onOrderPaid(OrderPaidEvent event) {
     Order order = orderRepo.findById(event.orderId()).orElseThrow();
     order.paid(event.ok());
--- src/main/java/victor/training/modulith/payment/PaymentGatewayWebHookApi.java
+++ src/main/java/victor/training/modulith/payment/PaymentGatewayWebHookApi.java
@@ -2,6 +2,7 @@ package victor.training.modulith.payment;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,11 +14,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class PaymentGatewayWebHookApi { // TODO move to 'payment' module
 //  private final OrderModuleApi orderModule;
   private final ApplicationEventPublisher eventPublisher;
+
   @PutMapping("payment/{orderId}/status")
+  @Transactional
   public String confirmPayment(@PathVariable long orderId, @RequestBody boolean ok) {
-//    orderModule.onOrderPaid(orderId, ok);
+    // unRepo.save(ceva);
     eventPublisher.publishEvent(new OrderPaidEvent(orderId, ok));
-    System.out.println("Exit");
     return "Payment callback received";
   }
 }
