6a5aa209a0fdc5704622fb96c95ffda23ed7cbbb
Victor Rentea <victorrentea@gmail.com>
2024-05-27 15:26:07 +0300

    events notes

--- src/main/java/victor/training/modulith/payment/OrderPaidEvent.java
+++ src/main/java/victor/training/modulith/payment/OrderPaidEvent.java
@@ -1,4 +1,7 @@
 package victor.training.modulith.payment;
 
+// daca platesc si altceva decat orderuri,
+// voi vorbi de PaymentStatusUpdatedEvent(paymentId, ok)
+// merita facut de pe acum...
 public record OrderPaidEvent(long orderId, boolean ok) {
 }
