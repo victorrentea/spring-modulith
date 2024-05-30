7d1372a7d93acdc1fe5e80986b4bbab0badd6b79
Victor Rentea <victorrentea@gmail.com>
2024-05-30 17:16:21 +0300

    eventuri ftw

--- src/main/java/victor/training/modulith/order/OrderModuleApi.java
+++ src/main/java/victor/training/modulith/order/OrderModuleApi.java
@@ -3,11 +3,16 @@ package victor.training.modulith.order;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.event.TransactionPhase;
+import org.springframework.transaction.event.TransactionalEventListener;
 import victor.training.modulith.order.impl.Order;
 import victor.training.modulith.order.impl.OrderRepo;
 import victor.training.modulith.payment.PaymentCompletedEvent;
 import victor.training.modulith.shipping.in.api.ShippingModuleApi;
+
+import static org.springframework.transaction.event.TransactionPhase.AFTER_COMMIT;
 
 @Slf4j
 @Service
@@ -16,7 +21,10 @@ public class OrderModuleApi {
   private final OrderRepo orderRepo;
   private final ShippingModuleApi shippingModule;
 
-  @EventListener
+  @TransactionalEventListener(phase = AFTER_COMMIT)
+  @Async
+
+  // @KafkaListener 🏆
   public  void onOrderPaid(PaymentCompletedEvent event) {
     Order order = orderRepo.findById(event.orderId()).orElseThrow();
     order.paid(event.ok());
