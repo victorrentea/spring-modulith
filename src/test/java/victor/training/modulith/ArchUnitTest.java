691fefc3f0b494c4f7c1f4b205c5c6da196e9477
Victor Rentea <victorrentea@gmail.com>
2024-07-11 01:02:54 +0300

    .matching("victor.training.modulith.(*)")

--- src/test/java/victor/training/modulith/ArchUnitTest.java
+++ src/test/java/victor/training/modulith/ArchUnitTest.java
@@ -4,7 +4,6 @@ import com.tngtech.archunit.core.domain.JavaClasses;
 import com.tngtech.archunit.core.importer.ClassFileImporter;
 import com.tngtech.archunit.library.dependencies.SliceRule;
 import com.tngtech.archunit.library.dependencies.SlicesRuleDefinition;
-import com.tngtech.archunit.library.dependencies.syntax.GivenSlices;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
@@ -16,7 +15,7 @@ class ArchUnitTest {
   private JavaClasses classes = new ClassFileImporter().importPackages("victor.training.modulith");
 
   @Test
-  @Disabled ("For after the APIs are moved to shared")
+  @Disabled("For after the APIs are moved to shared")
   public void sharedApiIndependent() {
     SliceRule sliceRule = SlicesRuleDefinition.slices()
         .matching("victor.training.modulith.shared.api.(*)..*")
@@ -34,7 +33,7 @@ class ArchUnitTest {
   @Test
   public void noCyclesBetweenModules() {
     SlicesRuleDefinition.slices()
-        .matching("victor.training.modulith.(*)..*")
+        .matching("victor.training.modulith.(*)")
         .should().beFreeOfCycles()
         .check(classes);
   }
