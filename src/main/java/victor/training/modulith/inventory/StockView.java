a5a89a8fb8eaef7f6e5e6bf0a3e157b6fa0bab3d
Victor Rentea <victorrentea@gmail.com>
2024-07-16 21:39:59 +0300

    comments

--- src/main/java/victor/training/modulith/inventory/StockView.java
+++ src/main/java/victor/training/modulith/inventory/StockView.java
@@ -6,17 +6,19 @@ import lombok.Getter;
 import org.hibernate.annotations.Immutable;
 import org.hibernate.annotations.View;
 
+// ⚠️ The very last resort!
+//   Use only when you really have to join this
+//   to a query of another module to avoid N+1 queries
 @Entity
 @View(query = """
     select STOCK.PRODUCT_ID, STOCK.ITEMS as STOCK
     from STOCK
     """)
 @Getter
-@Immutable // is never INSERT/UPDATE/DELETE
+@Immutable // cannot be INSERT/UPDATE/DELETE
 public class StockView { // part of the PUBLIC API of Inventory Module
+  // these fields are the contract of Inventory Modules
   @Id
   private long productId;
-
-  // these fields contract of Inventory Modules
   private Integer stock;
 }
