6b6028745393abb1bda1bf528d0f06ae7dc7bd4c
Victor Rentea <victorrentea@gmail.com>
2024-09-05 12:40:14 +0300

    #respectat incapsularea modulului order

--- src/main/java/victor/training/modulith/order/OrderInternalApi.java
+++ src/main/java/victor/training/modulith/order/OrderInternalApi.java
@@ -3,9 +3,14 @@ package victor.training.modulith.order;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import victor.training.modulith.order.impl.OrderService;
 
 @Service
 @RequiredArgsConstructor
 public class OrderInternalApi {
+  private final OrderService orderService;
 
+  public void confirmPayment(long orderId, boolean ok) {
+    orderService.confirmPayment(orderId, ok);
+  }
 }
