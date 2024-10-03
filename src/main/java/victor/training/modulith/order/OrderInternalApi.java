56b0d199241751c8fef3a5dbe4a9b19a7b7d9b42
Victor Rentea <victorrentea@gmail.com>
2024-10-03 18:36:16 +0300

    Revert "Events (here: via memory, you: kafka/rabbit)"
    This reverts commit 1fb82a21bb74866eb305afd888d14057aa3131cc.

--- src/main/java/victor/training/modulith/order/OrderInternalApi.java
+++ src/main/java/victor/training/modulith/order/OrderInternalApi.java
@@ -1,34 +1,11 @@
 package victor.training.modulith.order;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.context.event.EventListener;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-import victor.training.modulith.inventory.InventoryInternalApi;
-import victor.training.modulith.order.impl.Order;
-import victor.training.modulith.order.impl.OrderRepo;
-import victor.training.modulith.payment.PaymentConfirmedEvent;
-import victor.training.modulith.shipping.ShippingInternalApi;
 
 @Service
 @RequiredArgsConstructor
 public class OrderInternalApi {
-  private final OrderRepo orderRepo;
-  private final InventoryInternalApi inventoryInternalApi;
-  private final ShippingInternalApi shippingInternalApi;
-
-  // @KafkaListener
-  // @RabbitListener
-  @EventListener // in memory event bus. don't use
-  public void onPaymentConfirmed(PaymentConfirmedEvent event) {
-    Order order = orderRepo.findById(event.orderId()).orElseThrow();
-    order.pay(event.ok());
-    if (order.status() == OrderStatus.PAYMENT_APPROVED) {
-      inventoryInternalApi.confirmReservation(order.id());
-      String trackingNumber = shippingInternalApi.requestShipment(order.id(), order.shippingAddress());
-      order.wasScheduleForShipping(trackingNumber);
-    }
-    orderRepo.save(order);
-  }
-
 
 }
--- src/main/java/victor/training/modulith/order/impl/PaymentService.java
+++ src/main/java/victor/training/modulith/order/impl/PaymentService.java
@@ -1,4 +1,4 @@
-package victor.training.modulith.payment;
+package victor.training.modulith.order.impl;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -9,12 +9,9 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class PaymentService {
   private final PaymentGatewayClient paymentGatewayClient;
-//  static Map<Long,Runnable> paymentCallbacks = new HashMap<>();
-  public String generatePaymentUrl(
-      long orderId,
-      double total) { // TODO move to 'payment' module
+
+  public String generatePaymentUrl(long orderId, double total) { // TODO move to 'payment' module
     // payment gateway implementation details
-//    paymentCallbacks.put(orderId, r);
     log.info("Request payment url for order id: " + orderId);
     String gatewayUrl = paymentGatewayClient.generatePaymentLink("order/" + orderId + "/payment-accepted", total, "modulith-app");
     return gatewayUrl + "&orderId=" + orderId;
