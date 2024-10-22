ee35f86e837fb80c086ee6b484c6049aada1b0a2
Victor Rentea <victorrentea@gmail.com>
2024-10-22 12:00:58 +0300

    encapsulated

--- src/main/java/victor/training/modulith/payment/PaymentInternalApi.java
+++ src/main/java/victor/training/modulith/payment/PaymentInternalApi.java
@@ -3,6 +3,7 @@ package victor.training.modulith.payment;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import victor.training.modulith.payment.impl.PaymentGatewayClient;
 
 @Slf4j
 @Service
