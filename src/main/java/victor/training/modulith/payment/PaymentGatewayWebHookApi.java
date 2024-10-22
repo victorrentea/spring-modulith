0f00b1fb2ae7452e93a17e956d5bbb74aef83973
Victor Rentea <victorrentea@gmail.com>
2024-10-22 11:59:43 +0300

    event-based. you know what Kris is gonna tell us.

--- src/main/java/victor/training/modulith/payment/PaymentGatewayWebHookApi.java
+++ src/main/java/victor/training/modulith/payment/PaymentGatewayWebHookApi.java
@@ -2,6 +2,7 @@ package victor.training.modulith.payment;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -18,10 +19,12 @@ import victor.training.modulith.shipping.ShippingInternalApi;
 @RequiredArgsConstructor
 // Webhook = a call back to me over HTTP
 public class PaymentGatewayWebHookApi { // TODO move to 'payment' module
-  private final OrderInternalApi orderInternalApi;
+//  private final OrderInternalApi orderInternalApi;
+  private final ApplicationEventPublisher eventPublisher;
   @PutMapping("payment/{orderId}/status")
   public String confirmPayment(@PathVariable long orderId, @RequestBody boolean ok) {
-    orderInternalApi.confirmPayment(orderId, ok);
+//    orderInternalApi.confirmPayment(orderId, ok);
+    eventPublisher.publishEvent(new PaymentConfirmedEvent(orderId, ok));
     return "Payment callback received";
   }
 }
