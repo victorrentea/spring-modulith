42cd3f0e0b2ae478d2a3655bc4c5df093d900f26
Victor Rentea <victorrentea@gmail.com>
2024-10-22 11:54:37 +0300

    dependecy inversion: extracted an interface from the 'lower-level', 'less complex', 'less imoportant' module and place it the module I want to reduce the coupling of

--- src/main/java/victor/training/modulith/order/OrderInternalApi.java
+++ src/main/java/victor/training/modulith/order/OrderInternalApi.java
@@ -1,7 +1,6 @@
 package victor.training.modulith.order;
 
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import victor.training.modulith.inventory.InventoryInternalApi;
 import victor.training.modulith.order.impl.Order;
