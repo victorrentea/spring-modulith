423fd4a55fe21d0ad1806e834660d664ffcca6da
Victor Rentea <victorrentea@gmail.com>
2024-10-24 08:11:41 +0300

    events debate

--- src/main/java/victor/training/modulith/catalog/impl/Product.java
+++ src/main/java/victor/training/modulith/catalog/impl/Product.java
@@ -26,6 +26,39 @@ public class Product {
   private Double stars;
 
   private Boolean inStock;
+  // Stock-service has to keep up to date catalog-service/Product.inStock
+// event design: what name and contents?
+// -ProductsInStock{List<productId> max1000;} 😔
+// -StockIncremented/StockDecremented 😔 = not DOMAIN words/ubiquitous language
+// -StockDecreased
+// -StockUpdated{productId} 😔notification requiring a call back
+//=======  😀
+// A) StockUpdated{productId, stock} = <CUD Event> moving a data snapshot
+// Pro: extensibility (more consumers can listen later) = integration event
+
+// Domain Events:
+// B) OutOfStock{productId} + BackInStock{productId} = convenient for catalog
+// Pro: less messages
+// Cons: consumer-specific ?
+
+// C) ItemSold{productId, quantity} / StockRefilled / StockAdjusted{delta}
+//      >>> instead of ItemReturned / ItemDroppedAndSmashedOnTheGround / ItemLostFromWarehouse
+// Cons: force consumer has to track old state (past stock)
+// Cons: not idempotent = not naturally retryable > consumer has to deduplicate by other means (eg headers)
+  // + field private Integer stock;
+
+// D) ItemSold{productId, quantity, stockSnapshot:{items:N, warehouse}} = EventShot
+// Pro: consumers don't have to remember past state
+// Cons: you leak your internal data
+
+// with a [stateful] event stream processor (KStream)
+// you can convert A [or C] to B
+
+
+
+
+
+
 
   @OneToMany(mappedBy = "product")
   private List<ProductReview> reviews = new ArrayList<>();
