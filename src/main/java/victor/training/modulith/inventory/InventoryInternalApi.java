57502d34b4d1bccedabebf05128662ac89efc589
Victor Rentea <victorrentea@gmail.com>
2024-12-06 13:03:13 +0200

    respected encapsulation. Green tests

--- src/main/java/victor/training/modulith/inventory/InventoryInternalApi.java
+++ src/main/java/victor/training/modulith/inventory/InventoryInternalApi.java
@@ -2,7 +2,9 @@ package victor.training.modulith.inventory;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import victor.training.modulith.inventory.model.Stock;
 import victor.training.modulith.inventory.repo.StockRepo;
+import victor.training.modulith.inventory.repo.StockReservationRepo;
 import victor.training.modulith.inventory.service.StockService;
 import victor.training.modulith.shared.LineItem;
 
@@ -13,6 +15,7 @@ import java.util.List;
 public class InventoryInternalApi {
   private final StockService stockService;
   private final StockRepo stockRepo;
+  private final StockReservationRepo stockReservationRepo;
 
   public void reserveStock(long orderId, List<LineItem> items) {
     stockService.reserveStock(orderId, items);
@@ -22,4 +25,13 @@ public class InventoryInternalApi {
     stockService.confirmReservation(orderId);
   }
 
+  public int getAvailableStock(long productId) {
+    return stockRepo.findByProductId(productId).map(Stock::items).orElse(0);
+//        - stockReservationRepo.findByProductId(productId).stream().mapToInt(Stock::items).sum();
+
+    // PANIC: StockReservation
+    // when do they do stock -= reservedStock
+    // a) at reservation time
+    // a) after payment
+  }
 }
