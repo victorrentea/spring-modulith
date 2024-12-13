365717893f88a7b070bf1568c0646cea063747fd
Victor Rentea <victorrentea@gmail.com>
2024-12-13 16:09:53 +0200

    Events. avoid in a single process.
    They make code harder to maintain/navigate/understand.
    
    only use before extracting one module as a microservice

---
+++ src/main/java/victor/training/modulith/payment/PaymentConfirmedEvent.java
@@ -0,0 +1,4 @@
+package victor.training.modulith.payment;
+
+public record PaymentConfirmedEvent(long orderId, boolean ok) {
+}
--- src/main/java/victor/training/modulith/payment/PaymentGatewayWebHookApi.java
+++ src/main/java/victor/training/modulith/payment/PaymentGatewayWebHookApi.java
@@ -2,25 +2,22 @@ package victor.training.modulith.payment;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import victor.training.modulith.inventory.InventoryInternalApi;
-import victor.training.modulith.order.OrderInternalApi;
-import victor.training.modulith.order.impl.OrderRepo;
-import victor.training.modulith.shipping.ShippingInternalApi;
 
 @Slf4j
 @RestController
 @RequiredArgsConstructor
 // Webhook = a call back to me over HTTP
 public class PaymentGatewayWebHookApi { // TODO move to 'payment' module
-  private final OrderInternalApi orderInternalApi;
+  private final ApplicationEventPublisher eventPublisher;// kafkaTemplate
 
   @PutMapping("payment/{orderId}/status")
   public String confirmPayment(@PathVariable long orderId, @RequestBody boolean ok) {
-    orderInternalApi.confirmPayment(orderId, ok);
+    eventPublisher.publishEvent(new PaymentConfirmedEvent(orderId, ok));
     return "Payment callback received";
   }
 }
