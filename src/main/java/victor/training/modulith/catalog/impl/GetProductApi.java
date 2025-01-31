2366bc2685d553958a45d2b27de404164e346450
Victor Rentea <victorrentea@gmail.com>
2025-01-31 15:50:40 +0200

    arch test failed

--- src/main/java/victor/training/modulith/catalog/impl/GetProductApi.java
+++ src/main/java/victor/training/modulith/catalog/impl/GetProductApi.java
@@ -5,19 +5,22 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import victor.training.modulith.inventory.model.Stock;
+import victor.training.modulith.inventory.repo.StockRepo;
 
 @Slf4j
 @RestController
 @RequiredArgsConstructor
 public class GetProductApi {
   private final ProductRepo productRepo;
+  private final StockRepo stockRepo;
   // example of Vertical Slice Architecture (VSA) - one class / API
 
   public record GetProductResponse(
       long id,
       String name,
       String description,
-//       int stock, // TODO display stock in product page UI
+       int stock, // TODO display stock in product page UI
       Double price,
       Double stars
       ) {
@@ -26,11 +29,11 @@ public class GetProductApi {
   @GetMapping("catalog/{productId}")
   public GetProductResponse execute(@PathVariable long productId) {
     Product product = productRepo.findById(productId).orElseThrow();
-    //int stock= TODO
+    int stock= stockRepo.findById(productId).map(Stock::items).orElse(0);
     return new GetProductResponse(product.id(),
         product.name(),
         product.description(),
-//         stock,
+         stock,
         product.price(),
         product.stars()
     );
--- src/main/java/victor/training/modulith/shipping/in/internalapi/package-info.java
+++ src/main/java/victor/training/modulith/shipping/in/internalapi/package-info.java
@@ -1,4 +1,4 @@
 @NamedInterface
-package victor.training.modulith.shipping.in.api;
+package victor.training.modulith.shipping.in.internalapi;
 // this package + sub-packages is the shipping internal API
 import org.springframework.modulith.NamedInterface;
