263d6661b53a00057e7b7032581d8d3be6d79a69
Victor Rentea <victorrentea@gmail.com>
2025-01-31 17:26:49 +0200

    events in memory

--- src/main/java/victor/training/modulith/order/OrderInternalApi.java
+++ src/main/java/victor/training/modulith/order/OrderInternalApi.java
@@ -1,10 +1,12 @@
 package victor.training.modulith.order;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Service;
 import victor.training.modulith.inventory.InventoryInternalApi;
 import victor.training.modulith.order.impl.Order;
 import victor.training.modulith.order.impl.OrderRepo;
+import victor.training.modulith.payment.PaymentCompletedEvent;
 import victor.training.modulith.shipping.ShippingInternalApi;
 
 @Service
@@ -14,9 +16,10 @@ public class OrderInternalApi {
   private final InventoryInternalApi inventoryInternalApi;
   private final ShippingInternalApi shippingInternalApi;
 
-  public void confirmPayment(long orderId, boolean ok) {
-    Order order = orderRepo.findById(orderId).orElseThrow();
-    order.pay(ok);
+  @EventListener
+  public void onPaymentCompleted(PaymentCompletedEvent event) {
+    Order order = orderRepo.findById(event.orderId()).orElseThrow();
+    order.pay(event.ok());
     if (order.status() == OrderStatus.PAYMENT_APPROVED) {
       inventoryInternalApi.confirmReservation(order.id());
       String trackingNumber = shippingInternalApi.requestShipment(order.id(), order.shippingAddress());
@@ -24,4 +27,5 @@ public class OrderInternalApi {
     }
     orderRepo.save(order);
   }
+
 }
--- src/main/java/victor/training/modulith/payment/PaymentGatewayWebHookApi.java
+++ src/main/java/victor/training/modulith/payment/PaymentGatewayWebHookApi.java
@@ -2,24 +2,25 @@ package victor.training.modulith.payment;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import victor.training.modulith.order.OrderInternalApi;
 
 @Slf4j
 @RestController
 @RequiredArgsConstructor
 // Webhook = a call back to me over HTTP
 public class PaymentGatewayWebHookApi { // TODO move to 'payment' module
-  private final OrderInternalApi IOrderInternalApi;
+  private final ApplicationEventPublisher eventPublisher;
 
   @PutMapping("payment/{orderId}/status")
   public String confirmPayment(@PathVariable long orderId, @RequestBody boolean ok) {
-    IOrderInternalApi.confirmPayment(orderId, ok);
+//    IOrderInternalApi.confirmPayment(orderId, ok);
+//    eventPublisher.publishEvent(new PaymentEvent(orderId, ok)); // bad event name
+//    eventPublisher.publishEvent(new PaymentStatusEventUpdated(orderId, ok)); // CUD; boring event
+    eventPublisher.publishEvent(new PaymentCompletedEvent(orderId, ok)); // CUD; boring event
     return "Payment callback received";
   }
-
-
 }
