458fa08f4602b732195ab43a31f0abd041df2d3b
Victor Rentea <victorrentea@gmail.com>
2025-04-30 15:56:58 +0300

    from here, next maven module

--- src/main/java/victor/training/modulith/catalog/impl/ProductRepo.java
+++ src/main/java/victor/training/modulith/catalog/impl/ProductRepo.java
@@ -5,7 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface ProductRepo extends JpaRepository<Product, Long> {
   List<Product> searchByNameLikeIgnoreCase(String namePart, PageRequest pageRequest);
--- src/main/java/victor/training/modulith/catalog/impl/SearchApi.java
+++ src/main/java/victor/training/modulith/catalog/impl/SearchApi.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import victor.training.modulith.inventory.InventoryInternalApi;
-import victor.training.modulith.inventory.repo.StockRepo;
 
 import java.util.List;
 
