a11240fd7d7e6f9f864db6dc557367e8c99fc7d3
Victor Rentea <victorrentea@gmail.com>
2025-05-16 13:21:43 +0300

    format

--- src/main/java/victor/training/modulith/inventory/StockReservationRequestKnob.java
+++ src/main/java/victor/training/modulith/inventory/StockReservationRequestKnob.java
@@ -5,5 +5,7 @@ import victor.training.modulith.shared.LineItem;
 import java.util.List;
 
 // "Knob" = Internal Dto 😆
-public record StockReservationRequestKnob(long orderId, List<LineItem> items) {
+public record StockReservationRequestKnob(
+    long orderId,
+    List<LineItem> items) {
 }
