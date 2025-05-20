b7cf74135a80e752d28945e1c3c7ffb27c5ae823
Victor Rentea <victorrentea@gmail.com>
2025-05-20 18:28:27 +0300

    Revert "generates ascii doc"
    This reverts commit e547d7efab96939afaacd7ff78615747fe8d7b62.

--- shared/src/main/java/victor/training/modulith/shared/api/order/package-info.java
+++
@@ -1,4 +0,0 @@
-@NamedInterface
-package victor.training.modulith.shared.api.order;
-import org.springframework.modulith.NamedInterface;
-// allow other modules to access this package + its sub-packages
