f532c6026f522722d31c857ecd9beb3d1b78b1f5
Victor Rentea <victorrentea@gmail.com>
2025-05-20 16:12:13 +0300

    cycle

--- src/main/java/victor/training/modulith/order/OrderInternalApi.java
+++ src/main/java/victor/training/modulith/order/OrderInternalApi.java
@@ -1,11 +1,27 @@
 package victor.training.modulith.order;
 
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import victor.training.modulith.inventory.InventoryInternalApi;
+import victor.training.modulith.order.impl.Order;
+import victor.training.modulith.order.impl.OrderRepo;
+import victor.training.modulith.shipping.ShippingInternalApi;
 
 @Service
 @RequiredArgsConstructor
 public class OrderInternalApi {
+  private final OrderRepo orderRepo;
+  private final ShippingInternalApi shippingInternalApi;
+  private final InventoryInternalApi inventoryInternalApi;
 
+  public void confirmPayment(long orderId, boolean ok) {
+    Order order = orderRepo.findById(orderId).orElseThrow();
+    order.pay(ok);
+    if (order.status() == OrderStatus.PAYMENT_APPROVED) {
+      inventoryInternalApi.confirmReservation(order.id());
+      String trackingNumber = shippingInternalApi.requestShipment(order.id(), order.shippingAddress());
+      order.wasScheduleForShipping(trackingNumber);
+    }
+    orderRepo.save(order);
+  }
 }
---
+++ src/main/java/victor/training/modulith/payment/PaymentGatewayWebHookApi.java
@@ -0,0 +1,25 @@
+package victor.training.modulith.payment;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import victor.training.modulith.order.OrderInternalApi;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+// Webhook = a call back to me over HTTP
+public class PaymentGatewayWebHookApi { // TODO move to 'payment' module
+
+  private final OrderInternalApi orderInternalApi;
+
+  @PutMapping("payment/{orderId}/status")
+  public String confirmPayment(@PathVariable long orderId, @RequestBody boolean ok) {
+    orderInternalApi.confirmPayment(orderId, ok);
+    return "Payment callback received";
+  }
+
+}
