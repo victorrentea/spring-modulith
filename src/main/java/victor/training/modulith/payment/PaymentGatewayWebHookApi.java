a680d1300a7e6a6aca8bff57c5846ce2c91fdb77
Victor Rentea <victorrentea@gmail.com>
2025-05-21 11:11:50 +0200

    moved the order workflow RESPONSIBILITY back to the order module. Also this way I #respected the encapsulation of the order module.   #raise x 2

--- src/main/java/victor/training/modulith/payment/PaymentGatewayWebHookApi.java
+++ src/main/java/victor/training/modulith/payment/PaymentGatewayWebHookApi.java
@@ -6,33 +6,19 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import victor.training.modulith.inventory.InventoryInternalApi;
-import victor.training.modulith.order.OrderStatus;
-import victor.training.modulith.order.impl.Order;
-import victor.training.modulith.order.impl.OrderRepo;
-import victor.training.modulith.shipping.ShippingInternalApi;
+import victor.training.modulith.order.OrderInternalApi;
 
 @Slf4j
 @RestController
 @RequiredArgsConstructor
 // Webhook = HTTP call back to me from a third party
 public class PaymentGatewayWebHookApi { // TODO move to 'payment' module
-  private final OrderRepo orderRepo;
-  private final ShippingInternalApi shippingInternalApi;
-  private final InventoryInternalApi inventoryInternalApi;
+  private final OrderInternalApi orderInternalApi;
 
   @PutMapping("payment/{orderId}/status")
   public String confirmPayment(@PathVariable long orderId, @RequestBody boolean ok) {
-    Order order = orderRepo.findById(orderId).orElseThrow();
-    order.pay(ok);
-    if (order.status() == OrderStatus.PAYMENT_APPROVED) {
-      inventoryInternalApi.confirmReservation(order.id());
-      String trackingNumber = shippingInternalApi.requestShipment(order.id(), order.shippingAddress());
-      order.wasScheduleForShipping(trackingNumber);
-    } else {
-      inventoryInternalApi.cancelReservation(order.id());
-    }
-    orderRepo.save(order);
+    orderInternalApi.confirmPayment(orderId, ok);
     return "Payment callback received";
   }
+
 }
