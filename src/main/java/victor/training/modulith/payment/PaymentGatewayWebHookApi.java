a4b0f54223b5cf981261b7f31a8d8f923d0c9aa6
Victor Rentea <victorrentea@gmail.com>
2025-09-26 16:14:38 +0300

    pulled payment out, but moved order workflow logic back in order

--- src/main/java/victor/training/modulith/payment/PaymentGatewayWebHookApi.java
+++ src/main/java/victor/training/modulith/payment/PaymentGatewayWebHookApi.java
@@ -1,4 +1,4 @@
-package victor.training.modulith.order.impl;
+package victor.training.modulith.payment;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -7,30 +7,24 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import victor.training.modulith.inventory.InventoryInternalApi;
+import victor.training.modulith.order.OrderInternalApi;
 import victor.training.modulith.order.OrderStatus;
+import victor.training.modulith.order.impl.Order;
+import victor.training.modulith.order.impl.OrderRepo;
 import victor.training.modulith.shipping.ShippingInternalApi;
 
 @Slf4j
 @RestController
 @RequiredArgsConstructor
 // Webhook = HTTP call back to me from a third party
-public class PaymentGatewayWebHookApi { // TODO move to 'payment' module
-  private final OrderRepo orderRepo;
-  private final ShippingInternalApi shippingInternalApi;
-  private final InventoryInternalApi inventoryInternalApi;
+public class PaymentGatewayWebHookApi {
+  private final OrderInternalApi orderInternalApi; // TODO move to 'payment' module
 
   @PutMapping("payment/{orderId}/status")
   public String confirmPayment(@PathVariable long orderId, @RequestBody boolean ok) {
-    Order order = orderRepo.findById(orderId).orElseThrow();
-    order.pay(ok);
-    if (order.status() == OrderStatus.PAYMENT_APPROVED) {
-      inventoryInternalApi.confirmReservation(order.id());
-      String trackingNumber = shippingInternalApi.requestShipment(order.id(), order.shippingAddress());
-      order.wasScheduleForShipping(trackingNumber);
-    } else {
-      inventoryInternalApi.cancelReservation(order.id());
-    }
-    orderRepo.save(order);
+    orderInternalApi.confirm(orderId, ok);
     return "Payment callback received";
   }
+
+
 }
