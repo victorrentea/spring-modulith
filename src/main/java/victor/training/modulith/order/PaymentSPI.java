b5bb04732e7b1d3ebcd535f7bdad22d73cde0dc0
Victor Rentea <victorrentea@gmail.com>
2025-09-30 14:44:01 +0100

    dep inv

--- src/main/java/victor/training/modulith/order/PaymentSPI.java
+++ src/main/java/victor/training/modulith/order/PaymentSPI.java
@@ -1,5 +1,6 @@
 package victor.training.modulith.order;
 
 public interface PaymentSPI {
+  // Cycle fix #2: dep inversion using an interface
   String generatePaymentUrl(long orderId, double total);
 }
