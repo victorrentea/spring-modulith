352c949aaa43f9b4696415d541279373b9f53f62
Victor Rentea <victorrentea@gmail.com>
2025-12-16 15:53:09 +0200

    BAD. it passes my functional test but breaks architecture tests (manual ArchUnit and spring-modulith ones)

--- src/main/java/victor/training/modulith/catalog/impl/GetProductApi.java
+++ src/main/java/victor/training/modulith/catalog/impl/GetProductApi.java
@@ -5,12 +5,14 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import victor.training.modulith.inventory.repo.StockRepo;
 
 @Slf4j
 @RestController
 @RequiredArgsConstructor
 public class GetProductApi {
   private final ProductRepo productRepo;
+  private final StockRepo stockRepo;
 
   public record GetProductResponse(
       long id,
@@ -25,7 +27,7 @@ public class GetProductApi {
   @GetMapping("catalog/{productId}")
   public GetProductResponse getProduct(@PathVariable long productId) {
     Product product = productRepo.findById(productId).orElseThrow();
-    int stock = 0; // TODO display stock in the product details page in UI
+    int stock = stockRepo.findByProductId(product.id()).get().items();
     return new GetProductResponse(product.id(),
         product.name(),
         product.description(),
