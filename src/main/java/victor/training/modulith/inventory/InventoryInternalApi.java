c0ef41c4257739571378020e5e4b30e536713916
Victor Rentea <victorrentea@gmail.com>
2025-12-16 16:16:35 +0200

    Code review from inventory module SME

--- src/main/java/victor/training/modulith/inventory/InventoryInternalApi.java
+++ src/main/java/victor/training/modulith/inventory/InventoryInternalApi.java
@@ -3,6 +3,7 @@ package victor.training.modulith.inventory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import victor.training.modulith.inventory.model.Stock;
+import victor.training.modulith.inventory.model.StockReservation;
 import victor.training.modulith.inventory.repo.StockRepo;
 import victor.training.modulith.inventory.repo.StockReservationRepo;
 import victor.training.modulith.inventory.service.StockService;
@@ -27,6 +28,10 @@ public class InventoryInternalApi {
   }
 
   public int getStockByProduct(Long productId) {
-    return stockRepo.findByProductId(productId).map(Stock::items).orElse(0);
+    int reservedItems = stockReservationRepo.getStockReservationsByProductId(productId)
+        .stream()
+        .mapToInt(StockReservation::items)
+        .sum();
+    return stockRepo.findByProductId(productId).map(Stock::items).orElse(0) /*- reservedItems*/;
   }
 }
