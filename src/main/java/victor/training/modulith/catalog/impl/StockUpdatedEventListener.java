0193ddd7e1017b7d5eb7f0c60d083b4faef481db
Victor Rentea <victorrentea@gmail.com>
2026-02-03 17:53:44 +0200

    spring-modulith ftw

--- src/main/java/victor/training/modulith/catalog/impl/StockUpdatedEventListener.java
+++ src/main/java/victor/training/modulith/catalog/impl/StockUpdatedEventListener.java
@@ -3,9 +3,15 @@ package victor.training.modulith.catalog.impl;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
+import org.springframework.modulith.ApplicationModuleListener;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.event.TransactionPhase;
+import org.springframework.transaction.event.TransactionalEventListener;
 import victor.training.modulith.inventory.StockUpdatedEvent;
+
+import static org.springframework.transaction.event.TransactionPhase.AFTER_COMMIT;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -13,9 +19,21 @@ import victor.training.modulith.inventory.StockUpdatedEvent;
 public class StockUpdatedEventListener {
   private final ProductRepo productRepo;
 
-  @EventListener //  by default runs in publisher thread ± @Transaction 😱
+//  @EventListener //  by default runs in publisher thread ± @Transaction 😱
   // 𐀏 consistency
   // ⊖ error in listener rollbacks publishers' transaction😱 != message bbroker
+
+//  @Async // moves the listener on another thread + transaction❌
+  // ☢️Risk: listener might RUN in thread#2 BEFORE publisher thread#1 does COMMIT❌
+  // ☢️Risk: after publish, commit of publisher FAILS -> ROLLBACK
+
+  // only fire the event after the COMMIT of the publisher tx
+//  @TransactionalEventListener(phase = AFTER_COMMIT)
+//  @Async
+  // ☢️Risk: NOT DURABLE: if app crashes/shutdowm right before this metnhod runs ->
+
+
+  @ApplicationModuleListener // async + after commit + persisted in table until processed
   public void onEvent(StockUpdatedEvent event) {
     log.info("IN listener");
     Product product = productRepo.findById(event.productId()).orElseThrow();
