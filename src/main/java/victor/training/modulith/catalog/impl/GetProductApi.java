f4a123eec7e3081421083b829af80c105b77e382
Victor Rentea <victorrentea@gmail.com>
2026-03-06 12:22:36 +0200

    FE calling directly the other module's REST API

--- src/main/java/victor/training/modulith/catalog/impl/GetProductApi.java
+++ src/main/java/victor/training/modulith/catalog/impl/GetProductApi.java
@@ -5,7 +5,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 import victor.training.modulith.inventory.InventoryInternalApi;
+import victor.training.modulith.inventory.controller.GetStockApi;
 import victor.training.modulith.inventory.model.Stock;
 import victor.training.modulith.inventory.repo.StockRepo;
 
@@ -20,7 +22,11 @@ public class GetProductApi {
       long id,
       String name,
       String description,
-      int stock, // TODO
+      int stock, // TODO CR
+      // Push back on the user need, and DON'T change you BE contrac. instead:
+      // a) your FE after fetching the details, call inventory BE with fetch("/inventory/product/{id}/stock").
+      //   + 1 network call might hurt?
+      // b) Microfrontends: embed in your catalog FE page a <stock> FE compo made with❤️ by inventory team
       Double price,
       Double stars
   ) {
@@ -40,6 +46,17 @@ public class GetProductApi {
     //Can become a nightmare for developers that are landing in a minefield of complex business rules they don't have a clue about
     // Feature team (dev writing code💩 wherever necessary) vs Component Team/SME (maintaining at least review- of complex code)
 
+    // alternative: call the REST API of the other module
+//    int stock2 = new RestTemplate().getForObject("http://localhost:8080/stock/" + product.id(), Integer.class);
+    // + One canonical way to get the information. Don't repeat yourself. DRY principle
+    // - changes in their REST APIs will impact me
+    // - changes in internal api (more often, higher volatility) will impact REST API😱😱😱
+    // + if tomorrow I plan to eject 🚀 a module as a separate deployment
+    // - network call (but to localhost) ⇒ CPU work to [de]JSONify, failures😱
+    // - competing with actual external clients on HTTP-threads/sockets/memory
+    // - heavier testing 🤔, not just a Mockito(Java) / Moq|NSubstitute(C#) => WireMock|MockServer returning canned from :9999
+    // + ❤️❤️❤️ metrics
+//    Integer stock1 = getStockApi.getStock(product.id()); ❌❌❌❌
 
     return new GetProductResponse(product.id(),
         product.name(),
@@ -49,6 +66,7 @@ public class GetProductApi {
         product.stars()
     );
   }
+  private final GetStockApi getStockApi;
 }
 // Tip: stock is in inventory/impl/Stock#items
 // Tip: you are only allowed to use exposed classes of another modules
