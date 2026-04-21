65619a674a5022965cd348e754d0745528b7431f
Victor Rentea <victorrentea@gmail.com>
2026-04-21 22:55:57 +0300

    Revert "fail: no jOin between tables of 2 schema"
    This reverts commit 76873f73b6bb40b92302a3eb7c426a3859035b8e.

--- src/main/java/victor/training/modulith/catalog/impl/SearchApi.java
+++ src/main/java/victor/training/modulith/catalog/impl/SearchApi.java
@@ -6,8 +6,6 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import victor.training.modulith.inventory.InventoryInternalApi;
-import victor.training.modulith.inventory.repo.StockRepo;
 
 import java.util.List;
 
@@ -16,8 +14,6 @@ import java.util.List;
 @RequiredArgsConstructor
 public class SearchApi {
   private final ProductRepo productRepo;
-  private final StockRepo stockRepo;
-  private final InventoryInternalApi inventoryInternalApi;
 
   public record ProductSearchCriteria(String name, String description) { }
 
@@ -28,17 +24,8 @@ public class SearchApi {
       @RequestParam ProductSearchCriteria criteria,
       @RequestParam(required = false) PageRequest pageRequest) {
     // TODO only return items which are currently in stock
-
-//    var oom = productRepo.findAll();// 💥
-
     return productRepo.search(criteria.name, criteria.description, pageRequest)
         .stream()
-//        .filter(product -> stockRepo.findByProductId(product.id()).orElseThrow().items() > 0) ❌
-
-//        .filter(product -> inventoryInternalApi.getStockByProductId(product.id()) > 0) ❌
-        // ☠️A) N+1 QUERY PROBLEM : hitting DB in a LOOP: 20 + 1 => prefetch all stocks in memory
-        //    -> idsInStock.contains(product.id())
-        // ☠️B) filter after LIMIT => you show 15 elements not 20 on page #1
         .map(e -> new ProductSearchResult(e.id(), e.name()))
         .toList();
   }
