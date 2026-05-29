1362216b84151f70ecb17d8dd16812fccc0fce0e
Victor Rentea <victorrentea@gmail.com>
2026-05-29 15:49:35 +0300

    subsequent tx 😡 a fail in listener return 500 despite stock updated commited ✅ 😡 kill-9 by k8s make the 2nd tx not start=DATA LO$$

--- src/main/java/victor/training/modulith/catalog/impl/StockUpdatedEventListener.java
+++ src/main/java/victor/training/modulith/catalog/impl/StockUpdatedEventListener.java
@@ -4,6 +4,8 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.event.TransactionPhase;
+import org.springframework.transaction.event.TransactionalEventListener;
 import victor.training.modulith.inventory.StockUpdatedEvent;
 
 @Slf4j
@@ -12,11 +14,11 @@ import victor.training.modulith.inventory.StockUpdatedEvent;
 public class StockUpdatedEventListener {
   private final ProductRepo productRepo;
 
-  @EventListener
+  @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
   public void on(StockUpdatedEvent event) {
     Product product = productRepo.findById(event.productId()).orElseThrow();
     product.inStock(event.newStock() != 0);
-    if (true) throw new RuntimeException("BUG🐞");
+//    if (true) throw new RuntimeException("BUG🐞");
     productRepo.save(product);
   }
 }
