5384b3f44f898e8aecdec13a74de6221a9ad43da
Victor Rentea <victorrentea@gmail.com>
2026-05-29 16:17:52 +0300

    prep

--- src/main/java/victor/training/modulith/order/impl/PaymentGatewayWebHookApi.java
+++ src/main/java/victor/training/modulith/order/impl/PaymentGatewayWebHookApi.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import victor.training.modulith.inventory.InventoryInternalApi;
+import victor.training.modulith.order.OrderInternalApi;
 import victor.training.modulith.order.OrderStatus;
 import victor.training.modulith.shipping.ShippingInternalApi;
 
@@ -18,19 +19,13 @@ public class PaymentGatewayWebHookApi { // TODO move to 'payment' module
   private final OrderRepo orderRepo;
   private final ShippingInternalApi shippingInternalApi;
   private final InventoryInternalApi inventoryInternalApi;
+  private final OrderInternalApi orderInternalApi;
 
   @PutMapping("payment/{orderId}/status")
   public String confirmPayment(@PathVariable long orderId, @RequestBody boolean ok) {
-    Order order = orderRepo.findById(orderId).orElseThrow();
-    order.pay(ok);
-    if (order.status() == OrderStatus.PAYMENT_APPROVED) {
-      inventoryInternalApi.confirmReservation(order.id());
-      String trackingNumber = shippingInternalApi.requestShipment(order.id(), order.shippingAddress());
-      order.wasScheduleForShipping(trackingNumber);
-    } else {
-      inventoryInternalApi.cancelReservation(order.id());
-    }
-    orderRepo.save(order);
+    orderInternalApi.payOk(orderId, ok);
     return "Payment callback received";
   }
+
+
 }
