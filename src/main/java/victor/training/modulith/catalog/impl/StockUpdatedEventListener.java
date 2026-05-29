bcdbe0118894bd0e50a51de6863f0d4c55ccdb3d
Victor Rentea <victorrentea@gmail.com>
2026-05-29 16:16:54 +0300

    yolo

--- src/main/java/victor/training/modulith/catalog/impl/StockUpdatedEventListener.java
+++ src/main/java/victor/training/modulith/catalog/impl/StockUpdatedEventListener.java
@@ -3,6 +3,7 @@ package victor.training.modulith.catalog.impl;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
+import org.springframework.modulith.ApplicationModuleListener;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.event.TransactionPhase;
 import org.springframework.transaction.event.TransactionalEventListener;
@@ -14,7 +15,7 @@ import victor.training.modulith.inventory.StockUpdatedEvent;
 public class StockUpdatedEventListener {
   private final ProductRepo productRepo;
 
-  @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
+  @ApplicationModuleListener // spring-modulith inserts in the event in DB in the publisher tx, then picks it up for subscribers
   public void on(StockUpdatedEvent event) {
     Product product = productRepo.findById(event.productId()).orElseThrow();
     product.inStock(event.newStock() != 0);
